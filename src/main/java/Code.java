dda2a10be38ba7ff3d667a07e4932798f8f731cf
BSWaterB <50270641+bswaterb@users.noreply.github.com>
2020-06-16 22:32:06 +0800

    Update Code.java

--- src/main/java/Code.java
+++ src/main/java/Code.java
@@ -1,8 +1,5 @@
 import util.util2.ClipboardListener;
 
-/**
- * Created by chaohui on 2020/6/14
- */
 public class Code{
     public static void main(String[] args) throws Exception {
         ClipboardListener.Run();
